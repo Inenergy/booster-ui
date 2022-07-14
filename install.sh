546f1387fd3bf3e99319a6a07969e3907deb7a03
Alexander Efimov <efimokthefatass@gmail.com>
2022-07-14 15:14:12 +0300

    update installation instructions

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 npm i
 npm run build
 
-sudo echo "chromium-brower --incognito --app=http://localhost:6010" >> /etc/xdg/lxsession/LXDE-pi/autostart
+sudo echo "chromium-browser --incognito --app=http://localhost:6010" >> /etc/xdg/lxsession/LXDE-pi/autostart
 mkdir ~/.config/openbox
 cat > ~/.config/openbox/lxde-pi-rc.xml << EOF
-<?xml version="1.0" encoding="UTF-8">
+<?xml version="1.0" encoding="UTF-8"?>
 <openbox_config xmlns="http://openbox.org/3.4/rc">
 <applications>
 <application class="Chromium-browser">
@@ -22,11 +25,11 @@ After=network.target
 
 [Service]
 ExecStart=node server/main.js
-WorkingDirectory=/home/pi/booster-ui
+WorkingDirectory=/home/$LOGNAME/booster-ui
 StandardOutput=inherit
 StandardError=inherit
 Restart=always
-User=pi
+User=$LOGNAME
 
 [Install]
 WantedBy=multi-user.target
@@ -34,4 +37,12 @@ EOF
 
 sudo mv booster.service /etc/systemd/system/
 
-sudo systemctl enable booster.service #TODO create this service
+sudo systemctl enable booster.service
+
+mkdir config
+for i in settings initialize
+do
+    echo "{}" > config/"$i".json
+done
+
+echo "[]" > config/algorithm.json
