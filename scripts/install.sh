bbc44839119a73927207cb53347800f44c679bf8
Alexander Efimov <a.efimov@inenergy.ru>
2022-09-09 17:43:48 +0300

    separate install and autostart scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,50 +1,10 @@
-curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
+set -e
 
-cd ~/booster-ui
+#curl -fsSL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+#sudo apt-get install -y nodejs
+
 npm i
 npm run build
-
-mkdir ~/.config/autostart
-cat > ~/.config/autostart/booster-ui.desktop << EOF
-[Desktop Entry]
-Type=Application
-Exec=chromium-browser --incognito --app=http://localhost:6010
-EOF
-
-mkdir ~/.config/openbox
-cat > ~/.config/openbox/lxde-pi-rc.xml << EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<openbox_config xmlns="http://openbox.org/3.4/rc">
-<applications>
-<application class="Chromium-browser">
-<decor>no</decor>
-<maximized>yes</maximized>
-</application>
-</applications>
-</openbox_config>
-EOF
-
-cat > booster.service << EOF
-[Unit]
-Description=My service
-After=network.target
-
-[Service]
-ExecStart=node server/main.js
-WorkingDirectory=/home/$LOGNAME/booster-ui
-StandardOutput=inherit
-StandardError=inherit
-Restart=always
-User=$LOGNAME
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo mv booster.service /etc/systemd/system/
-
-sudo systemctl enable booster.service
 
 mkdir config
 for i in settings initialize
@@ -53,3 +13,5 @@ do
 done
 
 echo "[]" > config/algorithm.json
+
+source scripts/autostart.sh
